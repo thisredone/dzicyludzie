b1f55716426b04e6639a1b4587e21f160c8e9d02
Jack <damian.baran@currency-one.com>
2019-11-23 23:19:08 +0100

    background mp4

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app" class="subpixel-antialiased min-h-screen bg-dark-400 flex flex-col">
 
-    <div class="flex justify-center">
+    <video width="1200" height="1200" autoplay muted loop id="myVideo" class="z-0">
+      <source src="/background.mp4" type="video/mp4">
+    </video>
+
+    <div class="flex justify-center z-10">
       <h1 class="font-heading mb-8 tracking-widest" style="font-size: 12vw">
         DZICYLUDZIE
       </h1>
     </div>
 
-    <div class="flex justify-center items-center flex-grow -mt-24">
+    <div class="flex justify-center items-center flex-grow -mt-24 z-10">
 
       <div class="bg-mid-500 rounded-lg shadow-lg">
 
@@ -19,6 +23,7 @@
       </div>
     </div>
   </div>
+
 </template>
 
 
@@ -50,4 +55,15 @@ export default
   font-family 'Avenir', Helvetica, Arial, sans-serif
   color #2c3e50
 
+#myVideo {
+  position: fixed;
+  object-fit:cover;
+  width: 200%;
+  height: 100%;
+}
+
+#myBtn:hover {
+  background: #ddd;
+  color: black;
+}
 </style>
